bdc4c44a92fef4e6d2abc12b8d1def64dcb3a52c
agonzalez <agonzalez@mail.com>
2018-09-04 22:47:05 -0300

    [ComparePage] Correction when deleting Data Provider.

--- NopCommerce/src/tests/CompareTests.java
+++ NopCommerce/src/tests/CompareTests.java
@@ -12,21 +12,23 @@ import utils.DataProviders;
 public class CompareTests extends BaseTest {
 
 	private ComparePage comparePage;
+	
+	private static final String PRODUCT = "Digital";
 	private static final String COMPARE_MIN = "product comparison";
 	private static final String TITLE_SEARCH = "Search";
 	private static final String TITLE_COMPARE_PRODUCTS = "Compare products";
 	private static final String TEXT_EMPTY = "You have no items to compare.";
 
 	@Test(groups = { "ALL", "COMPARE" })
-	public void compareProductsTest(String product) {
+	public void compareProductsTest() {
 
 		assertTrue(comparePage.getLogo()); // Verifico que el logo de la empresa se encuentre desplegado en la pagina
 											// inicial.
 
-		comparePage.productSearch(product);
+		comparePage.productSearch(PRODUCT);
 		comparePage.clickSearch();
 
-		assertTrue(comparePage.getElementText().contains(product)); // Tomo la palabra del producto que utilice
+		assertTrue(comparePage.getElementText().contains(PRODUCT)); // Tomo la palabra del producto que utilice
 																	// para buscar y
 																	// la comparo con el nombre del elemento que
 																	// dio como resultado
@@ -37,10 +39,10 @@ public class CompareTests extends BaseTest {
 																// titulo.
 		comparePage.clickAddCompareFirst();
 		
-		comparePage.productSearch(product);
+		comparePage.productSearch(PRODUCT);
 		comparePage.clickSearch();
 
-		assertTrue(comparePage.getElementText().contains(product));
+		assertTrue(comparePage.getElementText().contains(PRODUCT));
 		
 		comparePage.clickAddCompareSecond();
 		assertEquals(comparePage.getTextNotifications(), COMPARE_MIN); // Comparo que el mensaje de la notificacion sea
@@ -49,21 +51,21 @@ public class CompareTests extends BaseTest {
 		assertEquals(comparePage.getTitlePage(), TITLE_COMPARE_PRODUCTS); // Verifico estar en la pagina correcta
 																			// mediante el titulo.
 
-		assertTrue(comparePage.getProductFirstName().contains(product)); // Comparo por separado que los productos sean
+		assertTrue(comparePage.getProductFirstName().contains(PRODUCT)); // Comparo por separado que los productos sean
 																			// correctos acorde
-		assertTrue(comparePage.getProductLastName().contains(product)); // a la busqueda realizada.
+		assertTrue(comparePage.getProductLastName().contains(PRODUCT)); // a la busqueda realizada.
 	}
 
-	@Test(dataProvider = "searchProvider", dataProviderClass = DataProviders.class, groups = { "ALL", "COMPARE" })
-	public void cleanListTest(String product) {
+	@Test(groups = { "ALL", "COMPARE" })
+	public void cleanListTest() {
 
 		assertTrue(comparePage.getLogo()); // Verifico que el logo de la empresa se encuentre desplegado en la pagina
 											// inicial.
 
-		comparePage.productSearch(product);
+		comparePage.productSearch(PRODUCT);
 		comparePage.clickSearch();
 
-		assertTrue(comparePage.getElementText().contains(product)); // Tomo la palabra del producto que utilice
+		assertTrue(comparePage.getElementText().contains(PRODUCT)); // Tomo la palabra del producto que utilice
 																	// para buscar y
 																	// la comparo con el nombre del elemento que
 																	// dio como resultado
