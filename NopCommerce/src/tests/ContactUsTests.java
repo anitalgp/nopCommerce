205653f4a18b1d712bae2a3eeb4f5665f7db0ae0
agonzalez <agonzalez@mail.com>
2018-09-04 22:18:50 -0300

    [ContactUS] Page initialization in ContactUsTests.

--- NopCommerce/src/tests/ContactUsTests.java
+++ NopCommerce/src/tests/ContactUsTests.java
@@ -3,8 +3,10 @@ package tests;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import pages.BasePage;
 import pages.ContactUsPage;
 
 public class ContactUsTests extends BaseTest {
@@ -16,23 +18,29 @@ public class ContactUsTests extends BaseTest {
 	private static final String MENSAJE = "Mensaje de prueba";
 	private static final String CONTACT_US_TITLE = "Contact Us";
 
+	@BeforeClass(alwaysRun = true)
+	public void initPages() {
+		contactUsPage = new ContactUsPage(driver);
+	}
+
 	@Test(groups = { "ALL", "CONTACT US" })
 	public void contactTest() {
 
-		assertTrue(contactUsPage.contactUsVisibility()); //Verifico que el link contact Us se encuentre visible
-														// antes de utilizarlo.
+		assertTrue(contactUsPage.contactUsVisibility()); // Verifico que el link contact Us se encuentre visible
+															// antes de utilizarlo.
 		contactUsPage.clickContactUs();
-		assertEquals(contactUsPage.getTitlePage(), CONTACT_US_TITLE); //Verifico que me encuentro en la pagina correcta
-																	//mediante la obtencion del titulo de la pagina.
+		assertEquals(contactUsPage.getTitlePage(), CONTACT_US_TITLE); // Verifico que me encuentro en la pagina correcta
+																		// mediante la obtencion del titulo de la
+																		// pagina.
 		contactUsPage.contactUsFillName(FNAME);
-		assertTrue(contactUsPage.emailFielsVisibility()); //Verifico que el campo del mail se encuentre visble
-														//antes de utilizarlo.
+		assertTrue(contactUsPage.emailFielsVisibility()); // Verifico que el campo del mail se encuentre visble
+															// antes de utilizarlo.
 		contactUsPage.contactUsFillEmail(EMAIL);
 		contactUsPage.contactUsFillEnquiry(MENSAJE);
-		assertTrue(contactUsPage.submitVisibility()); //verifico que el boton de Submit este visible en su lugar
-													//antes de utilizarlo.
+		assertTrue(contactUsPage.submitVisibility()); // verifico que el boton de Submit este visible en su lugar
+														// antes de utilizarlo.
 		contactUsPage.clickSubmit();
 
-		assertTrue(contactUsPage.successfulMassageContactUs()); //Compruebo que el mensaje sea satisfactorio.
+		assertTrue(contactUsPage.successfulMassageContactUs()); // Compruebo que el mensaje sea satisfactorio.
 	}
 }
