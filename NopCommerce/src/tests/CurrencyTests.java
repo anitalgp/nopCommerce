0b1d0fb8fa0f2a78257aad6280c2ef242a8db4cd
agonzalez <agonzalez@mail.com>
2018-09-04 21:05:28 -0300

    [HomePage] Add "HomePage" by changing the selectors and methods of "CurrenyPage". CurrencyPage was eliminated.

--- NopCommerce/src/tests/CurrencyTests.java
+++ NopCommerce/src/tests/CurrencyTests.java
@@ -5,44 +5,44 @@ import static org.testng.Assert.assertTrue;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import pages.CurrencyPage;
+import pages.HomePage;
 import utils.DataProviders;
 
 public class CurrencyTests extends BaseTest {
 
-	private CurrencyPage currencyPage;
+	private HomePage homePage;
 
 	private static final String DOLLAR = "$";
 	private static final String EURO = "€";
 
 	@BeforeClass(alwaysRun = true)
 	public void initPages() {
-		currencyPage = new CurrencyPage(driver);
+		homePage = new HomePage(driver);
 	}
 
 	@Test(dataProvider = "searchProvider", dataProviderClass = DataProviders.class, groups = { "ALL", "CURRENCY" })
 	public void currencyDollarTest(String product) {
 
-		assertTrue(currencyPage.getLogo()); // Verifico que el logo de la empresa se encuentre desplegado en la pagina
+		assertTrue(homePage.getLogo()); // Verifico que el logo de la empresa se encuentre desplegado en la pagina
 											// inicial.
-		currencyPage.clickCurrencyForm();
-		assertTrue(currencyPage.currencyVisibility()); // Verifico que se encuentre efectivamente visible
+		homePage.clickCurrencyForm();
+		assertTrue(homePage.currencyVisibility()); // Verifico que se encuentre efectivamente visible
 														// el boton del Currency antes de utilizarlo.
-		currencyPage.selectDollarCurrency();
-		assertTrue(currencyPage.getProductPrice().contains(DOLLAR)); // al tomar los valores de los filtros del precio y
+		homePage.selectDollarCurrency();
+		assertTrue(homePage.getProductPrice().contains(DOLLAR)); // al tomar los valores de los filtros del precio y
 																		// del primer producto que arroja la busqueda.
 	}
 
 	@Test(dataProvider = "searchProvider", dataProviderClass = DataProviders.class, groups = { "ALL", "CURRENCY" })
 	public void currencyEuroTest(String product) {
 
-		assertTrue(currencyPage.getLogo()); // Verifico que el logo de la empresa se encuentre desplegado en la pagina
+		assertTrue(homePage.getLogo()); // Verifico que el logo de la empresa se encuentre desplegado en la pagina
 											// inicial.
-		currencyPage.clickCurrencyForm();
-		assertTrue(currencyPage.currencyVisibility()); // Verifico que se encuentre efectivamente visible
+		homePage.clickCurrencyForm();
+		assertTrue(homePage.currencyVisibility()); // Verifico que se encuentre efectivamente visible
 														// el boton del Currency antes de utilizarlo.
-		currencyPage.selectEuroCurrency();
-		assertTrue(currencyPage.getProductPrice().contains(EURO)); // al tomar los valores de los filtros del precio y
+		homePage.selectEuroCurrency();
+		assertTrue(homePage.getProductPrice().contains(EURO)); // al tomar los valores de los filtros del precio y
 																	// del primer producto que arroja la busqueda.
 
 	}
